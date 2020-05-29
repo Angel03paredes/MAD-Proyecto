9b2b62ac2fb52fc171da739fea9b5e5cf0a16d19
Angel03paredes <42367005+Angel03paredes@users.noreply.github.com>
2020-05-29 17:43:09 -0500

    Create SP_CLIENTE.sql

---
+++ StoredProcedure/SP_CLIENTE.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE [dbo].[SP_CLIENTE]
+
+
+
+@nombre varchar(50),
+@apellidoP varchar(50),
+@apellidoM varchar(50),
+@domicilio varchar(50),
+@RFC varchar(13),
+@correo  varchar(30),
+@telcasa BIGINT,
+@telcel BIGINT,
+@Referencia varchar(30),
+@Fecha_NA datetime
+
+
+AS
+Select * from cliente
+INSERT INTO cliente(nombre,apellidop,apellidom,domicilio,rfc,correo,tel_casa,tel_cel,referencia,fecha_nac)
+VALUES(@nombre,@apellidoP,@apellidoM,@domicilio,@RFC,@correo,@telcasa,@telcel,@Referencia,@Fecha_NA)
